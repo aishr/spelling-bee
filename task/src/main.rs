84407114b3f99940e22fb5616ffdb332c2acafb7
Aishwarya Ramanathan <cutieice@gmail.com>
2022-05-19 19:37:45 -0400

    Houston, we have backend

--- task/src/main.rs
+++ task/src/main.rs
@@ -1,17 +1,22 @@
 use rand::prelude::*;
+use serde::Serialize;
 use std::collections::HashSet;
 use std::io::{BufReader, BufRead};
 use std::fs::File;
-fn main() {
-
-
-    let letters = generate_letters();
-    dbg!(&letters);
-    let words = get_words(letters);
-    dbg!(words);
+fn main()-> Result<(), std::io::Error> {
+    loop {
+        let (req,sel) = generate_letters();
+        dbg!(req, &sel);
+        let words = get_words(req, &sel)?;
+        if words.len() < 10{continue;}
+        dbg!(&words);
+        store_words(req, sel, words)?;
+        break;
+    } 
+    Ok(())
 }
 
-fn generate_letters() -> HashSet<char> {
+fn generate_letters() -> (char, HashSet<char>) {
     let mut sel_letters = HashSet::new();
     let mut rng = rand::thread_rng();
     while sel_letters.len() < 7 {
@@ -20,10 +25,12 @@ fn generate_letters() -> HashSet<char> {
         });
 
     }
-    sel_letters
+    let mut tmp = sel_letters.into_iter();
+
+    (tmp.next().unwrap(), tmp.collect())
 }
 
-fn get_words(sel_letters: HashSet<char>)-> std::io::Result<Vec<String>> {
+fn get_words(req:char, sel_letters: &HashSet<char>)-> std::io::Result<Vec<String>> {
     //let file = include_bytes!("./words.txt");
     let mut match_words = Vec::new();
     let file = File::open("./words.txt")?;
@@ -31,10 +38,39 @@ fn get_words(sel_letters: HashSet<char>)-> std::io::Result<Vec<String>> {
 
     for line in reader.lines() {
         let tmp_line = line?;
-        if tmp_line.len() > 3 && tmp_line.trim().chars().map(|c| sel_letters.contains(&c)).all(|b|b) {
+
+        if tmp_line.len() <= 3 {continue;}
+        let mut req_char = false;
+        let mut valid_word = true;
+        for c in tmp_line.chars() {
+            if req == c {
+                req_char = true;
+            } 
+            else if !sel_letters.contains(&c) {
+                valid_word = false;
+                break;
+            }
+        }
+        if valid_word && req_char {
             match_words.push(tmp_line);
         }
+        
     }
     Ok(match_words)
-    
+}
+#[derive(Serialize)]
+struct FileFormat {
+    req:String,
+    sel:Vec<String>,
+    words:Vec<String>
+}
+fn store_words(req:char, sel:HashSet<char>, match_words:Vec<String>)-> std::io::Result<()>{
+    let file = File::create("./match_words.json")?;
+    let result = FileFormat{
+        req:req.to_string(),
+        sel:Vec::from_iter(sel.iter().map(ToString::to_string)),
+        words:match_words
+    };
+    serde_json::to_writer(&file, &result)?;
+    Ok(())
 }
