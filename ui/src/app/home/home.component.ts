62dc68d993ac4c125c0d6d5e7ce2c5b90b25b1a6
Aishwarya Ramanathan <cutieice@gmail.com>
2022-05-21 23:49:19 -0400

    things are good but looks poopy, testing github actions

--- ui/src/app/home/home.component.ts
+++ ui/src/app/home/home.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import * as wordInfo from '../../../../task/match_words.json';
+interface MatchWords {
+  req: string,
+  sel: string[],
+  words: string[]
+}
 
 @Component({
   selector: 'app-home',
@@ -7,6 +13,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   inputValue: string = "";
+  wordData:MatchWords = wordInfo;
+  submittedWords:string[] = [];
 
   constructor() { }
 
@@ -14,8 +22,19 @@ export class HomeComponent implements OnInit {
     console.log("init");
   }
 
-  foo() {
-    console.log(this.inputValue);
+  validateWord() {
+    if (!this.submittedWords.includes(this.inputValue) && this.wordData.words.includes(this.inputValue)) {
+      this.submittedWords.push(this.inputValue);
+    }
+    this.inputValue = "";
+  }
+
+  backspace() {
+    this.inputValue = this.inputValue.slice(0, this.inputValue.length - 1);
+  }
+
+  shuffle() {
+    this.wordData.sel = this.wordData.sel.sort((a, b) => 0.5 - Math.random());
   }
 
   addToValue = (value:string) => {
